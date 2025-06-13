6804d477bff690210adeebcb2eab8249fab7319e
wayne <waynekim07@gmail.com>
2025-06-13 11:59:51 +0300

    Added the deleteFunc

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,38 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './index.css'
 import './App.css'
-import Button from './Components/Button/Button'
 import { CgAdd } from "react-icons/cg";
 import Todo from './Components/Todo/Todo';
 
 
 function App() {
   const [todos, setTodos] = useState([])
-  const [todoList,setTodosList] = useState([])
-  
-  
-
+  const todoList = useRef(null)
   useEffect(()=>renderTodos(),[todos])
-  const renderTodos = ()=>{ 
+  function renderTodos(){
     
-    setTodosList((tL)=>[...todos.map((item,index)=>{
-      return (
-        <Todo
-          paras={{item,index}}
-        />
-    )
-    })])
   }
   const addTodoFunc = ()=>{
-    setTodos(t=>[...t,{task:"new Todo", dueDate:""}])
+    setTodos(t=>[...t,{id:t.length+1,content:"new Todo",isEditing:false,isChecked:false}])
+  }
+  const editFunc=()=>{
+
+  }
+  const saveFunc=()=>{
+    
+  }
+  const cancelFunc=()=>{
+
   }
   
   return (
     <>
-  
-      <div className="app min-h-[100vh] grid place-content-center">
-        <div className="card w-[550px]">
-          <div className="heading">TODOs</div>
-          <div className="todos flex flex-col overflow-y-scroll  h-[200px] gap-3">
-            {todoList}
-          </div>
+      <div className="grid place-content-center w-[100vw] h-[100vh] bg-">
+        <div className="card w-[500px]">
+          <div className="heading">Todos</div>
+          <div className="todos my-3 h-[400px] overflow-y-scroll flex flex-col gap-1" ref={todoList}>{todos.map(task=><Todo paras={{task,setTodos}}/>)}</div>
           <div className="btn-section justify-center">
-            <Button 
-              paras={{buttonText:<CgAdd/>,toolTipText:"Add",func:addTodoFunc}}
-            />
+            <button className="btn " onClick={()=>addTodoFunc()}><CgAdd/></button>
           </div>
         </div>
       </div>
---
+++ src/Components/Todo/Todo.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useRef } from 'react'
+import { HiSaveAs } from "react-icons/hi";
+import { MdOutlineCancel,MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+function Todo({paras}) {
+  const {task,setTodos} = paras
+  const deleteFunc = id=> setTodos(t=>t.filter(task=>task.id !== id))
+  return(
+    <>
+      <li 
+        className='flex  items-center justify-between bg-[var(--bg-100)] px-4 py-2 rounded-lg'
+        key={task.id}
+      >
+          <div className="task-checkbox-section ">
+            <label className='flex items-center gap-2 cursor-pointer'>
+              <input 
+                type="checkbox" 
+                className="checkbox cursor-pointer " 
+                onChange={e=>checkFunc(e)}
+                defaultChecked={task.isChecked}
+              />
+              <div className="task">{task.content}</div>
+            </label>
+          </div>
+          <div className="btn-section gap-4">
+            <button 
+              className="btn edit-btn"
+              onClick={()=>editFunc()}
+            ><FaEdit/></button>
+            <button 
+              className="btn edit-btn "
+              onClick={()=>deleteFunc(task.id)}
+            ><MdDelete/></button>
+          </div>
+      </li>
+    </>
+  )
+
+}
+
+export default Todo
